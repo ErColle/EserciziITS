808dd00f61db40a410289377c849d09b845fa3c6
ErColle <alessandrocolella205@gmail.com>
2025-02-21 09:18:07 +0100

    gg

--- lezione_03/matchstatement.py
+++ lezione_03/matchstatement.py
@@ -34,3 +34,28 @@ match (a, b):
         print("a is 1 and b is 2")
     case (1, 1):
         print("a is 1 and b is 1")
+    
+# STATEMENT LISTE E DIZIONARI
+
+#LISTE
+lista = [1, 2, 3, 4, 5]
+
+match lista:
+    case [1, 2, 3]:
+        print("La lista contiene 1, 2 e 3")
+    case [1, 2, 3, 4, 5]:
+        print("La lista contiene 1, 2, 3, 4 e 5")
+    case _:
+        print("La lista non contiene 1, 2 e 3")
+        
+#DIZIONARI
+
+dizionario = {"nome": "Mario", "cognome": "Rossi", "età": 30}
+
+match dizionario:
+    case {"nome": "Mario", "cognome": "Rossi"}:
+        print("Il dizionario contiene Mario Rossi")
+    case {"nome": "Mario", "cognome": "Rossi", "età": 30}:
+        print("Il dizionario contiene Mario Rossi e ha 30 anni")
+    case _:
+        print("Il dizionario non contiene Mario Rossi")
