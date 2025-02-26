2e9323e17b7eca955a064c05544f6c9aecbdcee9
ErColle <alessandrocolella205@gmail.com>
2025-02-26 16:12:47 +0100

    z

--- lezione_03/es_cicli.py
+++ lezione_03/es_cicli.py
@@ -68,25 +68,57 @@
 
 # 4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
 
-i = 1
-list =[] 
-for i in range (1, 11):
-    list.append(i**3)
-    i += 1
+# i = 1
+# list =[] 
+# for i in range (1, 11):
+#     list.append(i**3)
+#     i += 1
 
-print(list)
+# print(list)
 
-# 4-10. Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
-# • Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
-# • Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
-# • Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+# # 4-10. Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+# # • Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+# # • Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
+# # • Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
 
-pizze = ["Margherita", "Diavola", "Quattro Stagioni", "Boscaiola", "Wustel e patatine", "Marinara", "Rossa", "Crostino", "Pepperoni"] 
+# pizze = ["Margherita", "Diavola", "Quattro Stagioni", "Boscaiola", "Wustel e patatine", "Marinara", "Rossa", "Crostino", "Pepperoni"] 
 
-# for pizze in pizze:
-#     print(f"I like {pizze} pizza.")
+# # for pizze in pizze:
+# #     print(f"I like {pizze} pizza.")
+# # print("I really love pizza!")
+
+# print(pizze[:3])
+# print(pizze[-3:])
+# print(pizze[3:6])
+
+
+# 4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. Make a copy of the list of pizzas, and call it friend_pizzas. 
+# Then, do the following:
+# • Add a new pizza to the original list.
+# • Add a different pizza to the list friend_pizzas.
+# • Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. 
+# Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. 
+# Make sure each new pizza is stored in the appropriate list.
+
+# pizze = ["Margherita", "Diavola", "Quattro Stagioni"]
+# for pizza in pizze:
+#     print(f"I like {pizza} pizza.")
 # print("I really love pizza!")
 
-print(pizze[:3])
-print(pizze[-3:])
-print(pizze[3:6])
+# pizze.append("Boscaiola")
+# pizza_friend =["Marinara", "Carbonara", "Gamberetti"]
+# print(f"My Favourite pizzas are:") 
+
+# for pizza in pizze:
+#     print(pizza)
+# print(f"My friend's favourite pizzas are:")
+# for pizza_friend in pizza_friend:
+#     print(pizza_friend)
+
+# 4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, to save space.
+# Choose a version of foods.py, and write two for loops to print each list of foods.
+
+
+# 4-14. PEP 8: Look through the original PEP 8 style guide at https://peps.python.org/pep-0008/. 
+# You won’t use much of it now, but it might be interesting to skim through it.
+
