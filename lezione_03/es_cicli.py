db9072753348aaafc55383f01c00dba768acdca0
ErColle <alessandrocolella205@gmail.com>
2025-03-04 09:31:40 +0100

    d

--- lezione_03/es_cicli.py
+++ lezione_03/es_cicli.py
@@ -122,3 +122,120 @@
 # 4-14. PEP 8: Look through the original PEP 8 style guide at https://peps.python.org/pep-0008/. 
 # You won’t use much of it now, but it might be interesting to skim through it.
 
+
+# 5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+# describing each test and your prediction for the results of each test. Your code
+# should look something like this:
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# • Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# • Create at least 10 tests. Have at least 5 tests evaluate to True and another
+# 5 tests evaluate to False.
+
+# animale = input("Inserisci un animale: ")
+# print("l'animale è un cane?")
+# if animale == "cane":
+#     print(True)
+# else:
+#     print(False)
+# print("l'animale è un gatto?")
+# if animale == "gatto":
+#     print(True)
+# else:
+#     print(False)
+# print("l'animale è un coniglio?")
+# if animale == "coniglio":
+#     print(True)
+# else:
+#     print(False)
+# print("l'animale è un cavallo?")
+# if animale == "cavallo":
+#     print(True)
+# else:
+#     print(False)
+
+
+# 5-2. More Conditional Tests: You don’t have to limit the number of tests you cre-
+# ate to 10. If you want to try more comparisons, write more tests and add them
+
+# to conditional_tests.py. Have at least one True and one False result for each of
+# the following:
+# • Tests for equality and inequality with strings
+# • Tests using the lower() method
+# • Numerical tests involving equality and inequality, greater than and less
+# than, greater than or equal to, and less than or equal to
+# • Tests using the and keyword and the or keyword
+# • Test whether an item is in a list
+# • Test whether an item is not in a list
+
+# 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+# • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+
+# 5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+# • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+# • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+# • Write one version of this program that runs the if block and another that runs the else block.
+
+# 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+
+# • If the alien is green, print a message that the player earned 5 points.
+# • If the alien is yellow, print a message that the player earned 10 points.
+# • If the alien is red, print a message that the player earned 15 points.
+# • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+
+# 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+# • If the person is less than 2 years old, print a message that the person is a baby.
+# • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+# • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# • If the person is age 65 or older, print a message that the person is an elder.
+
+# 5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+# • Make a list of your three favorite fruits and call it favorite_fruits.
+# • Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like Apples!
+
+# 5-8. Hello Admin: Make a list of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the list, and print a greeting to each user.
+# • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+# • Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.
+
+
+# 5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+# • If the list is empty, print the message We need to find some users!
+# • Remove all of the usernames from your list, and make sure the correct message is printed.
+
+# 5-10. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+# • Make a list of five or more usernames called current_users.
+# • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+# • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+# • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.)
+
+
+# 5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+# • Store the numbers 1 through 9 in a list.
+# • Loop through the list.
+# • Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+
+# 6-1. Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+# 6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary. Think of a favorite number for each person, and store each as a value in your dictionary. Print each person’s name and their favorite number. For even more fun, poll a few friends and get some actual data for your program.
+# 6-3. Glossary: A Python dictionary can be used to model an actual dictionary. However, to avoid confusion, let’s call it a glossary.
+# • Think of five programming words you’ve learned about in the previous chapters. Use these words as the keys in your glossary, and store their meanings as values.
+# • Print each word and its meaning as neatly formatted output. You might print the word followed by a colon and then its meaning, or print the word on one line and then print its meaning indented on a second line. Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+
+# 6-7. People: Start with the program you wrote for Exercise 6-1. Make two new dictionaries representing different people, and store all three dictionaries in a list called people. Loop through your list of people. As you loop through the list, print everything you know about each person.
+
+# 6-8. Pets: Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and as
+# you do, print everything you know about each pet. 
+
+# 6-9. Favorite Places: Make a dictionary called favorite_places. Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. To make this exercise a bit more interesting, ask some friends to name a few of their favorite places. Loop through the dictionary, and print each person’s name and their favorite places.
+# 6-10. Favorite Numbers: Modify your program from Exercise 6-2 so each person can have more than one favorite number. Then print each person’s name along with their favorite numbers.
+# 6-11. Cities: Make a dictionary called cities. Use the names of three cities as keys in your dictionary. Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city. The keys for each city’s dictionary should be something like country, population, and fact. Print the name of each city and all of the information you have stored about it.
+
+# 6-12. Extensions: We’re now working with examples that are complex enough that they can be extended in any number of ways. Use one of the example programs from this chapter, and extend it by adding new keys and values, changing the context of the program, or improving the formatting of the output.
---
+++ lezione_04/fuzioni.py
@@ -0,0 +1,21 @@
+#Sum of integers from 1 to  10 
+
+sum: int = 0 
+for i in range(1, 11):
+    sum += i
+print(f"Sum of integers from 1 to 10: {sum}")
+#Sum of integers from 20 37 
+
+sum: int = 0
+for i in range(20, 38):
+    sum += i
+print(f"Sum of integers from 20 to 37: {sum}")
+
+#Sum of integers from 35 to 49
+
+sum: int = 0 
+for i in range (35, 50):
+    sum += i
+print(f"Sum of integers from 35 to 49: {sum}")
+
+
