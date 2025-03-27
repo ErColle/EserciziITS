8a7640034df96300403af723da77ef7793be040c
ErColle <alessandrocolella205@gmail.com>
2025-03-27 16:30:10 +0100

    ff

--- lezione_06/Esercizi2.py
+++ lezione_06/Esercizi2.py
@@ -59,4 +59,25 @@ pokehouse.describe_restaurant()
 # Make another method called greet_user() that prints a personalized greeting to the user. 
 # Create several instances representing different users, and call both methods for each user.
 
+class User:
+    
+    def __init__(self, nome, cognome, eta, sesso):
+        self.nome = nome
+        self.cognome = cognome
+        self.eta = eta
+        self.sesso = sesso
+        
+    def describe_user(self):
+        print(f"Nome: {self.nome}\nCognome: {self.cognome}\nEta: {self.eta}\nSesso: {self.sesso}")
+        
+    def greet_user(self):
+        print(f"Ciao {self.nome} {self.cognome}!") 
+        
+
+alessandro = User("Alessandro", "Colella", 20, "M")
+    
+alessandro.describe_user()
+alessandro.greet_user()
+    
+
 # 9-4. Number Served: Start with your program from Exercise 9-1. Add an attribute called number_served with a default value of 0. Create an instance called restaurant from this class. Print the number of customers the restaurant has served, and then change this value and print it again. Add a method called set_number_served() that lets you set the number of customers that have been served. Call this method with a new number and print the value again. Add a method called increment_number_served() that lets you increment the number of customers who’ve been served. Call this method with any number you like that could represent how many customers were served in, say, a day of business. 
