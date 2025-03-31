aece9be1e15ddda32246c5b04f7ad84158274794
ErColle <alessandrocolella205@gmail.com>
2025-03-31 10:08:07 +0200

    h

--- lezione_06/classi.py
+++ lezione_06/classi.py
@@ -49,6 +49,35 @@ class Student:
     def getGroupAverage(cls):
         avg = sum(cls.studentGrades) / len(cls.studentGrades)#--> cls SERVE PER ACCEDERE ATTRIBUTI DI CLASSE (equivalente self) 
         return avg
+    
+#MEDOTO STR -> SERVE PER RAPPRESENTARE UNA CLASSE IN STRINGA
+#SE NON VIENE DEFINITO, OUTPUT STAMPA LA POSIZIONE IN MEMORIA DELLA CLASSE
+
+class Persona:
+    def __init__(self, nome, eta):
+        self.nome = nome
+        self.eta = eta
+
+    def __str__(self):
+        return f"{self.nome} ha {self.eta} anni"
+    
+p = Persona("Alice", 25)
+print(str(p))   # Alice ha 25 anni
+
+#METODO CALL -> SERVE PER RENDERE UN OGGETTO "CALLABLE" (COME UNA FUNZIONE)
+
+class Saluta:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __call__(self):
+        return f"Ciao, {self.nome}!"
+
+s = Saluta("Alice")
+print(s())  # Output: Ciao, Alice!
+
+
+
 
 
 
