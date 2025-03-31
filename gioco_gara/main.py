400aa0eceba3cfc840dd6b1578e423d2ab4d81ba
ErColle <alessandrocolella205@gmail.com>
2025-03-31 20:55:16 +0200

    AZ

--- gioco_gara/main.py
+++ gioco_gara/main.py
@@ -25,18 +25,117 @@ SE H ARRIVA POSIZIONE 70 == "HARE WINS || YUCH!!"
 
 import time 
 import random
+import os
 
-def gara(len_percorso):
+def gara():
     
-    percorso = ["_" * len_percorso]
+    timer = 0
     
-stringa = ["_" * 70]  
+    lunghezza_percorso = 70
+    percorso = ['_'] * lunghezza_percorso
 
-print("  ".join(stringa))
-    
 
-# def tartaruga_move():
+    posizione_tartaruga = 1
+    posizione_lepre = 1
     
+    percorso[posizione_lepre - 1] = "H"
+    percorso[posizione_tartaruga] = "T"
     
+    print("BANG !!!!! AND THEY'RE OFF !!!!!")
+    
+    print(" ".join(percorso))
+    
+    time.sleep(1)
+    
+    while True:
+        
+        os.system('cls')
+        
+        percorso = ['_'] * lunghezza_percorso
+        
+        percorso[posizione_lepre - 1] = "H"
+        percorso[posizione_tartaruga - 1] = "T"
+        
+        posizione_tartaruga = tartaruga_move(posizione_tartaruga)
+        
+        posizione_lepre = lepre_move(posizione_lepre)
+         
+         
+        if posizione_tartaruga == posizione_lepre:
+            percorso[posizione_lepre - 1] == "OUCH" 
+        
+        if posizione_tartaruga >= lunghezza_percorso:
+            print("LA TARTARUGA HA VINTO!!! YAY!!!")
+            break
+        elif posizione_lepre >= lunghezza_percorso:
+            print("LA LEPRE HA VINTO!!! YUCH!!!")
+            break
+        
+        print(f"Durata gara: {timer} secondi")
+        timer += 1
+        
+        print(f"Posizione lepre: {posizione_lepre}")
+        print(f"Posizione tartaruga: {posizione_tartaruga}")
+        
+        print(" ".join(percorso))
+        
+        time.sleep(1)
 
-# def lepre_move():
+
+'''
+
+- Passo veloce (50% di probabilità): avanza di 3 quadrati.
+    - Scivolata (20% di probabilità): arretra di 6 quadrati. Non può andare sotto il quadrato 1.
+    - Passo lento (30% di probabilità): avanza di 1 quadrato.
+
+
+'''
+
+def tartaruga_move(posizione_tartaruga):
+    numero = random.randint(1, 10)
+    
+    if numero <= 5:  
+        posizione_tartaruga += 3
+        
+    elif numero <= 8:  
+        posizione_tartaruga += 1
+        
+    else: 
+        posizione_tartaruga = max(1, posizione_tartaruga - 6)
+    
+    return posizione_tartaruga
+        
+'''
+
+  - Riposo (20% di probabilità): non si muove.
+    - Grande balzo (20% di probabilità): avanza di 9 quadrati.
+    - Grande scivolata (10% di probabilità): arretra di 12 quadrati. Non può andare sotto il quadrato 1.
+    -  Piccolo balzo (30% di probabilità): avanza di 1 quadrato.
+    - Piccola scivolata (20% di probabilità): arretra di 2 quadrati. Non può andare sotto il quadrato 1.
+
+
+'''
+
+
+def lepre_move(posizione_lepre):
+    numero = random.randint(1, 10)
+
+    if numero <= 2: 
+        posizione_lepre = max(1, posizione_lepre - 12)
+    
+    elif numero <= 4:
+        posizione_lepre += 9
+        
+    elif numero <= 6:  
+        posizione_lepre += 1
+        
+    elif numero <= 8: 
+        posizione_lepre = max(1, posizione_lepre - 2)
+        
+    else: 
+        pass  
+
+    return posizione_lepre
+
+
+gara()
