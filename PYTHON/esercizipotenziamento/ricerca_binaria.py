02f5981b0ea4a6808b43c7f0dc25ced53e26efa7
ErColle <alessandrocolella205@gmail.com>
2025-06-10 11:20:17 +0200

    com

--- PYTHON/esercizipotenziamento/ricerca_binaria.py
+++ PYTHON/esercizipotenziamento/ricerca_binaria.py
@@ -7,7 +7,7 @@ def ricerca_binaria(numeri: list[int] , num: int):
     i_centro = (ultimo_elem + primo_elem) // 2
     valore = numeri[i_centro] 
     
-    while ...:
+    while :
         
         if valore == num:
             return True
@@ -23,4 +23,27 @@ def ricerca_binaria(numeri: list[int] , num: int):
     return False
         
 print(ricerca_binaria([2, 4, 5, 6, 8, 9, 11, 13, 17], 3))
+
+""" soluzione 
+
+def binary_search(numeri: list, num: int):
+    
+    i, j = 0, -1
+    
+    mid = len(numeri) // 2 
+    
+    if numeri[mid] ==  num:
+        return True
+    
+    if numeri[mid] > num:
+        
+        j = mid - 1
+        binary_search( numeri[i:j], num )
+        
+    else:
+        
+        j = mid + 1
+        binary_search( numeri[i:j], num ) """
+        
+        
                   
