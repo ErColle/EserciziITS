2e96dd2fa58388a02100df128228e87f627c42f4
ErColle <alessandrocolella205@gmail.com>
2025-06-11 13:56:38 +0200

    com

--- PYTHON/esercizipotenziamento/merge_intervals.py
+++ PYTHON/esercizipotenziamento/merge_intervals.py
@@ -1,10 +1,26 @@
-def merge_intervals(intervals: str) -> str:
-    for couple in intervals:
-        
-        indice = intervals[couple].index()
-        new_list = []  
-        
-        if couple[1] == intervals[indice][0]:        
+def merge_intervals(intervals):
     
-intervals = [[1, 4], [4, 5]]
-merge_intervals(intervals) # restituisce [[1, 5]]
+    couple: list = [] 
+    new_list = [] 
+    
+    for i in range(len(intervals) - 1):
+        
+        if intervals[i]  != intervals[-1]:
+            
+            if intervals[i][1] >= intervals[i + 1][0]:
+                
+                couple = [intervals[i][0], intervals[i +1][1]]
+                
+                del intervals[i], intervals[i + 1]  
+                
+                new_list.append(couple) 
+            
+            else:
+                new_list.append(intervals[i]) 
+        
+        return new_list
+            
+                    
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+
+print(merge_intervals(intervals)) # restituisce [[1, 5]]
