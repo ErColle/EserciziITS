7be5d5eb10dbc3367c998012e9622ed08415f511
ErColle <alessandrocolella205@gmail.com>
2025-09-21 22:34:48 +0200

    com

--- PYTHON/gestionesistemaospedaliero/fattura.py
+++ PYTHON/gestionesistemaospedaliero/fattura.py
@@ -1,46 +1,29 @@
-from dottore import Dottore, dottore
-from paziente import paziente1, paziente2, paziente3
+from dottore import Dottore 
+from paziente import Paziente
 
-class Fattura(Dottore):
+class Fattura:
     
-    def __init__(self, pazienti: list, dottore: str):
-        self.__pazienti = list(pazienti)
-        self.__dottore = dottore
-        if self.isAValidDoctor() == True:
-            self.__fatture = len(self.__pazienti)
-            self.__salary = 0
-        else:
-            self.__fatture = None 
-            self.__salary = None 
-            self.__pazienti = None 
-            self.__dottore = None
+    def __init__(self, patient: list, doctor):
+        
+        if doctor.isAValidDoctor(doctor):
+            self.__fatture = len(patient)
+            self.__salary = 0 
+        else: 
+            self.__patient = None
+            self.__doctor = None
+            self.__fatture = None
+            self.__salary = None
+            print("Non è possibile creare la classe fattura poichè il dottore non è valido!")
+            
     def getSalary(self):
-        self.__salary = self.getParcel() * self.__fatture
+        
+        self.__salary = self.doctor.getParcel() * self.fatture
         return self.__salary
     
     def getFatture(self):
-        self.__fatture = len(self.__pazienti)
-        return self.__fatture
-    def addPatient(self, newPatient: str):
-        self.__pazienti.append(newPatient)
-        self.getFatture()
-        self.getSalary()
-        self.getSalary
         
-    def removePatient(self, idCode: str):
-        for paziente in self.__pazienti:
-            if paziente.getIdCode() == idCode:
-                self.__pazienti.remove(paziente)
-                self.getFatture()
-                self.getSalary()
-                print(f"Alla lista del Dottor {self.__dottore} è stato rimosso il paziente {idCode}.")
-                break
-            
-pazienti = [paziente1, paziente2, paziente3]
-fattura = Fattura(pazienti, dottore)
-
-
-    
+        self.__fatture = len(self.__patient)
+        return self.__fatture
         
             
             
