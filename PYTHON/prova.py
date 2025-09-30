d239b95844b8d51bed70bdb3ef8efcf0ffd13dea
ErColle <alessandrocolella205@gmail.com>
2025-09-30 10:48:44 +0200

    com

---
+++ PYTHON/prova.py
@@ -0,0 +1,10 @@
+def flatten_once(nested: list) -> list:
+    new_lista = []
+    for elem in nested:
+        if isinstance(elem, list):
+            new_lista.extend(elem)
+        else:
+            new_lista.append(elem)
+    return new_lista
+
+print(flatten_once([1, 2, 3, 4, [1, 2, 3, 5], [1, 2, 3]]))
