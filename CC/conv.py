6b146aa9058a07cb4e91f946acf6ed07a3728ba2
ErColle <alessandrocolella205@gmail.com>
2025-10-01 13:55:02 +0200

    com

--- CC/conv.py
+++ CC/conv.py
@@ -1,10 +1,30 @@
-def convert(hex):
-    hex = hex.replace(":", "")
-    hex = int(hex, 16)
-    print(hex)
-    return hex
+mess = "ciao messaggio"
 
+# traduzione messaggio in numeri
 
-modulo ="00:8c:c9:ea:f5:52:15:d5:33:79:f5:a6:ac:7e:5c:88:07:a7:f7:f6:13:a1:2a:e5:40:41:97:f1:e3:b1:90:8a:e3:81:60:a4:8b:0f:b7:91:dc:d4:9b:de:cc:72:09:57:a1:dc:bb:da:82:f7:19:d7:64:00:18:bc:e9:ca:2b:3d:cc:bd:bc:0a:88:62:73:cd:b7:71:cd:b7:48:bd:98:e5:9d:4d:b5:95:e6:5b:86:29:e4:18:05:7e:5a:f0:51:b0:2c:08:1e:86:f6:2f:fc:ae:4c:db:e4:9a:61:0f:56:e5:41:56:e4:73:30:18:46:59:58:5d:29:c0:29:ae:f9:b3:3f"
+mess = int(mess.encode('utf-8').hex(), 16)
 
-convert(modulo)
+print("MESSAGGIO CONVERTITO IN ESADECIMALE:", mess)
+
+# publicExponent: 3 (0x3) chiave pubblica
+# prendo il modulo dal file modulo.txt
+
+with open('modulo.txt') as m:
+    modulo = m.read()
+    
+# tolgo al modulo : e accapo
+modulo = modulo.replace(':', '')
+modulo = modulo.replace('\n', '')
+modulo = modulo.replace(' ', '')
+
+modulo = int(modulo, 16)
+
+print("\n\nMODULO:", modulo)
+
+# crittografo il messaggio con la il messaggio convertito in intero
+# per la potenza della chiave pubblica, per il modulo del modulo
+messaggio_crittografato = pow(mess, 3) % modulo
+
+print("\n\n\nmessaggio crittografato:", messaggio_crittografato)
+
+# decodifico il messaggio con la chiave privata
