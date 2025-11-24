50361bf82d2f91e1cb9ddf85a03d367960798eae
ErColle <alessandrocolella205@gmail.com>
2025-11-24 09:15:42 +0100

    com

--- PYTHON/esercizi_generali/bubblesort.py
+++ PYTHON/esercizi_generali/bubblesort.py
@@ -1,16 +1,16 @@
-def bubble_sort(list):
+def bubble_sort(lista):
     
     for _ in range(len(list)):
         for i in range(len(list)):
             if i == len(list)-1:
                 break
             
-            if list[i] > list[i+1]:
-                list[i], list[i+1] = list[i+1], list[i]
+            if lista[i] > lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
    
     return list
 
-list = [34, 7, 23, 32, 5, 62, 32, 32]
+lista = [34, 7, 23, 32, 5, 62, 32, 32]
 
-print(bubble_sort(list))
+print(bubble_sort(lista))
     
